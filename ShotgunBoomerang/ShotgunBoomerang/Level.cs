26a35609e42be0a37943dcc5822a87d01b6977f8
Auden-Clifford <awc6002@rit.edu>
2023-03-06 16:44:33 -0500

    initialized Level class

---
+++ ShotgunBoomerang/ShotgunBoomerang/Level.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShotgunBoomerang
+{
+    internal class Level
+    {
+        // Fields
+        private List<Tile> _tileMap;
+
+        // Properties
+        /// <summary>
+        /// Gets the level's tile map
+        /// </summary>
+        public List<Tile> TileMap { get { return _tileMap; } }
+
+        /// <summary>
+        /// Creates a new level with a given tile map
+        /// </summary>
+        /// <param name="tileMap">The list of tiles the level will have</param>
+        public Level(List<Tile> tileMap)
+        {
+            _tileMap = tileMap;
+        }
+    }
+}
