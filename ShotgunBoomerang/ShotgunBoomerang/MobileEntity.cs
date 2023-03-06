6d9faa7b2832293fa864001687e5c4d5e0ae0741
Auden-Clifford <awc6002@rit.edu>
2023-03-06 15:37:04 -0500

    GameObject and MobileEntity initialized

---
+++ ShotgunBoomerang/ShotgunBoomerang/MobileEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShotgunBoomerang
+{
+    internal abstract class MobileEntity : GameObject
+    {
+        // Fields
+        private Vector2 _velocity;
+        private float _health;
+        private float _maxHealth;
+        private float _damage;
+
+
+        // Properties
+        public float Health { get { return _health; } set { _health = Math.Clamp(value, 0, _maxHealth); } }
+    }
+}
