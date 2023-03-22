157ccc0e744158fa63963ccc0cea97577f30d8ec
Auden-Clifford <112361970+Auden-Clifford@users.noreply.github.com>
2023-03-21 23:25:18 -0400

    Initialized Player Class

--- ShotgunBoomerang/ShotgunBoomerang/IGameProjectile.cs
+++ ShotgunBoomerang/ShotgunBoomerang/IGameProjectile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,33 @@ using System.Threading.Tasks;
 
 namespace ShotgunBoomerang
 {
-    internal interface Interface1
+    /// <summary>
+    /// An interface that describes the methods
+    /// common to all projectiles in the game. 
+    /// This should allow the program to treat 
+    /// all of the different projectiles as the same.
+    /// </summary>
+    internal interface IGameProjectile
     {
+        /// <summary>
+        /// Should tell the given spritebatch to draw/animate the projectile
+        /// </summary>
+        public void Draw(SpriteBatch sb);
+
+        /// <summary>
+        /// Should contain logic for the movement and physics interactions of the projectile
+        /// </summary>
+        public void Update();
+
+        /// <summary>
+        /// Should check if the projectile is colliding with any game objects
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckCollision();
+
+        /// <summary>
+        /// Should contain logic for what to do when the projectile hits a player or enemy
+        /// </summary>
+        public void hit();
     }
 }
---
+++ ShotgunBoomerang/ShotgunBoomerang/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShotgunBoomerang
+{
+    internal class Player : MobileEntity
+    {
+        // Fields
+        // Properties
+        // Constructors
+        // Methods
+    }
+}
